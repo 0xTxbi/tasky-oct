f97bf327d88d443362b34afc70c44f7a62dde094
TechieJossy <techieJossy@users.noreply.github.com>
2020-05-11 14:11:19 +0100

    Selected major components with JavaScript

---
+++ app.js
@@ -0,0 +1,7 @@
+// Declare necessary variables
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');

7f1d2c26f6e39d3250bb71760a0801854e3f7db4
TechieJossy <techieJossy@users.noreply.github.com>
2020-05-11 14:37:39 +0100

    Add tasks capabilities included with JavaScript

--- app.js
+++ app.js
@@ -5,3 +5,24 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// Load all Event Listeners
+loadEventListeners();
+
+// Load all Event Listeners
+function loadEventListeners() {
+	
+	form.addEventListener('submit', addTask);
+
+}
+
+function addTask(e) {
+	
+	if (taskInput.value == '') {
+		alert('Add a task');
+	}
+
+	//Create li elements once triggered
+
+	e.preventDefault();
+}
